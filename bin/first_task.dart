0cc8d3ba18d1f1d3bd1dbf1840eb4a61d7d5c2bf
Mostafa <mkamel202064@gmail.com>
2026-04-13 02:21:12 +0200

    second Commit add some comments

--- bin/first_task.dart
+++ bin/first_task.dart
@@ -1,6 +1,14 @@
+/*
+* Second session Tasks
+* date: 13/4/2026
+*First Task: write a program to print my
+* name in the terminal and push it to a repo */
 
+// the project main is the start point
 void main(){
+  // create a var that get my name
   String myName = "Mostafa Kamel";
 
+  // order the prog to print my name
   print('My name is: $myName');
 }
